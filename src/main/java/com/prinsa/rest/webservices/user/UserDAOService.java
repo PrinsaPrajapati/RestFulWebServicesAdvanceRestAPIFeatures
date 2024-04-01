2cfb0cf064197b4be29adc3bd33c01e33149e673
Prinsa Prajapati <prinsa.prajapati@gmail.com>
2024-04-01 12:06:40 -0700

    Implementing Generic Exception Handling for all Resources.  add-CustomizedResponseEntityExceptionHandler class  add-ErrorDetails class  --test through the Postman  GET http://localhost:8080/users/1000 with unknown user Id  --The response shows a Customized Message Structure  {     "timestamp": "2024-04-01T12:06:07.6286044",     "message": "Id: 1000",     "details": "uri=/users/1000" }

--- src/main/java/com/prinsa/rest/webservices/user/UserDAOService.java
+++ src/main/java/com/prinsa/rest/webservices/user/UserDAOService.java
@@ -16,9 +16,9 @@ public class UserDAOService {
 		private static List<User> users = new ArrayList<>();
 		private static int usersCount = 0;
 		static {
-			users.add(new User(1,"Angel",LocalDate.now().minusYears(30)));
-			users.add(new User(2,"Eve",LocalDate.now().minusYears(25)));
-			users.add(new User(3,"Jim",LocalDate.now().minusYears(20)));
+			users.add(new User(++usersCount,"Angel",LocalDate.now().minusYears(30)));
+			users.add(new User(++usersCount,"Eve",LocalDate.now().minusYears(25)));
+			users.add(new User(++usersCount,"Jim",LocalDate.now().minusYears(20)));
 		}
 		
 		public List<User> findAll() {
@@ -33,7 +33,7 @@ public class UserDAOService {
 
 		public User findOne(int id) {
 			Predicate<? super User> predicate = user -> user.getId().equals(id); 
-			return users.stream().filter(predicate).findFirst().get();
+			return users.stream().filter(predicate).findFirst().orElse(null);
 		}
 
 }
--- src/main/java/com/prinsa/rest/webservices/user/UserResourcesController.java
+++ src/main/java/com/prinsa/rest/webservices/user/UserResourcesController.java
@@ -1,37 +1,51 @@
 package com.prinsa.rest.webservices.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResourcesController {
 
-   @Autowired
+	@Autowired
 	private UserDAOService service;
 
-
-	// GET /users    http://localhost:8080/users
+	// GET /users http://localhost:8080/users
 	@GetMapping("/users")
 	public List<User> retrieveAllUsers() {
 		return service.findAll();
 	}
 
-	// GET /users   http://localhost:8080/users/1
+	// GET /users http://localhost:8080/users/1
 	@GetMapping("/users/{id}")
 	public User retrieveUser(@PathVariable int id) {
-		return service.findOne(id);
+		User user=service.findOne(id);
+		if(user==null) {
+			throw new UserNotFoundException("Id: " + id);
+		}
+		return user;
+	
 	}
 
-	//POST /users
+	// POST /users http://localhost:8080/users
 	@PostMapping("/users")
-	public void createUser(@RequestBody User user) {
-		service.save(user);
+	public ResponseEntity<Object> createUser(@RequestBody User user) {
+		User savedUser = service.save(user);
+
+		URI location = ServletUriComponentsBuilder
+				.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(savedUser.getId())
+				.toUri();
+
+		return ResponseEntity.created(location).build();
 	}
-	
+
 }
