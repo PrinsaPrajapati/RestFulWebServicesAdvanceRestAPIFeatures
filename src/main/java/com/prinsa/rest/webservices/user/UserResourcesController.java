b97f8a045cf530c63e2e0cd9271e1471bb7ae1c0
Prinsa Prajapati <prinsa.prajapati@gmail.com>
2024-04-01 16:07:36 -0700

    -- Implementing HATEOAS for REST API
    -- GET http://localhost:8080/users/1 with HATEOAS
    {
        "name": "Adam",
        "birthDate": "1992-08-19",
        "_links": {
            "all-users": {
                "href": "http://localhost:8080/users"
            }
        }
    }

--- src/main/java/com/prinsa/rest/webservices/user/UserResourcesController.java
+++ src/main/java/com/prinsa/rest/webservices/user/UserResourcesController.java
@@ -4,6 +4,8 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 import jakarta.validation.Valid;
 
@@ -27,16 +30,34 @@ public class UserResourcesController {
 		return service.findAll();
 	}
 
-	// GET /users http://localhost:8080/users/1
-	@GetMapping("/users/{id}")
-	public User retrieveUser(@PathVariable int id) {
-		User user=service.findOne(id);
-		if(user==null) {
-			throw new UserNotFoundException("Id: " + id);
-		}
-		return user;
-	
+	/*
+	 * // GET /users http://localhost:8080/users/1
+	 * 
+	 * @GetMapping("/users/{id}") public User retrieveUser(@PathVariable int id) {
+	 * User user=service.findOne(id); if(user==null) { throw new
+	 * UserNotFoundException("Id: " + id); } return user;
+	 * 
+	 * }
+	 */
+	//http://localhost:8080/users
+	//EntityModel
+	//WebMvcLinkBuilder
+		
+	@GetMapping("/users/{id}") // http://localhost:8080/users/1
+	public EntityModel<User> retrieveUser1(@PathVariable int id) {
+		User user = service.findOne(id);
+		
+		if(user==null)
+			throw new UserNotFoundException("id:"+id);
+		
+		EntityModel<User> entityModel = EntityModel.of(user);
+		
+		WebMvcLinkBuilder link =  linkTo(methodOn(this.getClass()).retrieveAllUsers());
+		entityModel.add(link.withRel("all-users"));
+		
+		return entityModel;
 	}
+	
 
 	// POST /users http://localhost:8080/users
 	@PostMapping("/users")
