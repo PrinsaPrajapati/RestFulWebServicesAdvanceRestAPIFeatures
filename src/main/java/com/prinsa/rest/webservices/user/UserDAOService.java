10725052a51c623fc2731872d11aa0c5ee2b8404
Prinsa Prajapati <prinsa.prajapati@gmail.com>
2024-04-01 12:11:56 -0700

    Implementing DELETE Method to delete a User Resource
    --UserDaoService.java Modified ,create DeletUser method
    
    --UserResourceController.java Modified ,create DeletUser method

--- src/main/java/com/prinsa/rest/webservices/user/UserDAOService.java
+++ src/main/java/com/prinsa/rest/webservices/user/UserDAOService.java
@@ -11,29 +11,33 @@ import org.springframework.stereotype.Repository;
 public class UserDAOService {
 
 	// JPA/Hibernate > Database
-		// UserDaoService > Static List
-		
-		private static List<User> users = new ArrayList<>();
-		private static int usersCount = 0;
-		static {
-			users.add(new User(++usersCount,"Angel",LocalDate.now().minusYears(30)));
-			users.add(new User(++usersCount,"Eve",LocalDate.now().minusYears(25)));
-			users.add(new User(++usersCount,"Jim",LocalDate.now().minusYears(20)));
-		}
-		
-		public List<User> findAll() {
-			return users;
-		}
+	// UserDaoService > Static List
 
-		public User save(User user) {
-			user.setId(++usersCount);
-			users.add(user);
-			return user;
-		}
+	private static List<User> users = new ArrayList<>();
+	private static int usersCount = 0;
+	static {
+		users.add(new User(++usersCount, "Angel", LocalDate.now().minusYears(30)));
+		users.add(new User(++usersCount, "Eve", LocalDate.now().minusYears(25)));
+		users.add(new User(++usersCount, "Jim", LocalDate.now().minusYears(20)));
+	}
 
-		public User findOne(int id) {
-			Predicate<? super User> predicate = user -> user.getId().equals(id); 
-			return users.stream().filter(predicate).findFirst().orElse(null);
-		}
+	public List<User> findAll() {
+		return users;
+	}
 
+	public User save(User user) {
+		user.setId(++usersCount);
+		users.add(user);
+		return user;
+	}
+
+	public User findOne(int id) {
+		Predicate<? super User> predicate = user -> user.getId().equals(id);
+		return users.stream().filter(predicate).findFirst().orElse(null);
+	}
+
+	public void deleteById(int id) {
+		Predicate<? super User> predicate = user -> user.getId().equals(id);
+		users.removeIf(predicate);
+	}
 }
--- src/main/java/com/prinsa/rest/webservices/user/UserResourcesController.java
+++ src/main/java/com/prinsa/rest/webservices/user/UserResourcesController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -48,4 +49,8 @@ public class UserResourcesController {
 		return ResponseEntity.created(location).build();
 	}
 
+	@DeleteMapping("/users/{id}")
+	public void deleteUser(@PathVariable int id) {
+		service.deleteById(id);
+	}
 }
